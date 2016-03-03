5c1eb0ffb557c92660a00a08505a5fba4409cb8a
Yan.Huang <ryer.huang@brightcells.com>
2016-03-04 02:18:13 +0800

    move js out

---
+++ public/javascripts/script.js
@@ -0,0 +1,19 @@
+// config
+var appid = 'wx37009e136e4b43ae';
+var secrect = '2897d54cf9cf571f9ff6efe3317cbfa5';
+var accessTokenUrl = [
+  'https://api.weixin.qq.com/cgi-bin/token',
+  '?grant_type=client_credential',
+  '&appid=', appid,
+  '&secret=', secrect
+].join('');
+
+// get access token
+$.ajax({
+  url: accessTokenUrl,
+  type: 'GET',
+  datatype: 'jsonp',
+  success: function(accessToken) {
+    alert(accessToken);
+  }
+});
